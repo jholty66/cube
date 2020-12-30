670e61f30dac4f8aaf32cfc7de1656a161de63bd
jholty66 <burholtcharliegmail.com>
2020-12-30 22:22:23 +0000

    pieces have own class, add function to check if solved

--- python/cube.py
+++ python/cube.py
@@ -1,7 +1,38 @@
 #!/usr/bin/env python3.9
 
+'''
+TODO:
+Test the turn function to see that it works on all puzzles.
+
+Function to check weather a face is solved.  Do this by looking at
+(for example) all stickers with value 0 and check that they all share
+the same colour.  Then extend this to check all faces to know weather
+the puzzle is solved.
+
+Function to scramble cube.  Later make scrambles WCA legal.
+
+(Maybe) function to display cube as plaintext. Faster to check
+functions that change state of cube.
+
+Search for 3D graphics librarty to display shapes.
+
+RESEARCH:
+Look at the rubiks cube solver that is written in CPP.
+ '''
+
 import random
 from typing import *
+
+def _join_lists(*lists: List[Any]) -> List[Any]:
+    # Mypy cannot joint mulitple lists of different types with +,
+    # even when the result is List[Any] or Union[A, B, C].  This is
+    # a 'fix'.
+    '''Type safe alternative (current version of Mypy) of joining
+two lists with "+".'''
+    joined_list: List[Any]
+    for lst in lists:
+        joined_list += lst
+    return joined_list
 
 class Sticker:
     def __init__(self, pos: int, depth: int):
@@ -9,82 +40,122 @@ class Sticker:
         self.pos = pos
         self.depth = depth
 
-class Solid:
-    def spawn_centers(self) -> List[List[Sticker]]:
-        return [[Sticker(i, 1)] for i in range(len(self.adjMat))] if self.order % 2 == 1 else []
+class Piece:
+    def __init__(self, *stickers: Sticker):
+        self.stickers = list(stickers)
 
-    def spawn_edges(self) -> List[List[Sticker]]:
-        edges: List[List[Sticker]] = []
-        r = range(1, int((self.order + 1) / 2))
+    def colour(self) -> List[int]:
+        return [sticker.colour for sticker in self.stickers]
+
+    def pos(self) -> List[int]:
+        return [sticker.pos for sticker in self.stickers]
+
+    def depth(self) -> List[int]:
+        return [sticker.depth for sticker in self.stickers]
+
+    def show(self):
+        print(self.colour(), self.pos(), self.depth())
+
+class Center(Piece):
+    def __init__(self, s1):
+        super().__init__(s1)
+
+class Edge(Piece):
+    def __init__(self, s1, s2):
+        super().__init__(s1, s2)
+
+class Corner(Piece):
+    def __init__(self, s1, s2, s3):
+        super().__init__(s1, s2, s3)
+
+class Solid:
+    def spawn_centers(self) -> List[Center]:
+        return [Center(Sticker(i, int((self.order + 1) / 2))) for i in range(self.faces)] if self.order % 2 == 1 else []
+
+    def spawn_edges(self) -> List[Edge]:
+        edges: List[Edge] = []
         if self.order % 2 == 1:
             for i in range(len(self.adjMat)):
                 for j in range(len(self.adjMat[i])):
-                    for depth1 in r:
-                        for depth2 in r:
-                            if 1 in (depth1, depth2):
-                                edge = [Sticker(i, depth1), Sticker(self.adjMat[i][j], depth2)]
-                                if [set(s.pos for s in edge), [s.depth for s in edge]] not in [[set(S.pos for S in e), [S.depth for S in e]] for e in edges]:
-                                    edges.append(edge)
+                    for d1 in range(1, int((self.order + 1) / 2)):
+                        for d2 in range(1, int((self.order + 1) / 2)):
+                            for d3 in range(1, int((self.order + 1) / 2)):
+                                if 1 in (d1, d2):
+                                    edge = Edge(Sticker(i, d1), Sticker(self.adjMat[i][j], d2))
+                                    if [set(edge.pos()), edge.depth()] not in [[EDGE.pos, EDGE.depth] for EDGE in edges]:
+                                        edges.append(edge)
         return edges
 
-    def spawn_corners(self) -> List[List[Sticker]]:
-        corners: List[List[Sticker]] = []
-        r = range(1 , int(self.order / 2) + 1)
+    def spawn_corners(self) -> List[Corner]:
+        corners: List[Corner] = []
         for i in range(len(self.adjMat)):
             for j in range(len(self.adjMat[i])):
-                for depth1 in r:
-                    for depth2 in r:
-                        for depth3 in r:
-                            if 1 in (depth1, depth2, depth3):
-                                corner = [Sticker(i, depth1), Sticker(self.adjMat[i][j - 1], depth2), Sticker(self.adjMat[i][j], depth3)]
-                                if [set(s.pos for s in corner), [s.depth for s in corner]] not in [[set(S.pos for S in c), [S.depth for S in c]] for c in corners]:
+                for d1 in range(1, int(self.order / 2) + 1):
+                    for d2 in range(1, int(self.order / 2) + 1):
+                        for d3 in range(1, int(self.order / 2) + 1):
+                            if 1 in (d1, d2, d3):
+                                corner = Corner(Sticker(i, d1), Sticker(self.adjMat[i][j - 1], d2), Sticker(self.adjMat[i][j], d3))
+                                if [set(corner.pos()), corner.depth()] not in [[CORNER.pos, CORNER.depth] for CORNER in corners]:
                                     corners.append(corner)
+
         return corners
 
-    def __init__(self, order: int, sides: int, faces: int, adjMat: List[List[int]]):
+    def findOppFaces(self):
+        op = dict()
+        for i in range(self.faces):
+            ff = [i]
+            for j in ff:
+                for k in self.adjMat[j]:
+                    if k not in ff:
+                        ff.append(k)
+                if len(ff) == len(self.adjMat) - 1:
+                    d = [f for f in range(self.faces) if f not in ff]
+                    op[i] = d[0]
+                    break
+        return op
+    
+    def __init__(self, order: int, adjMat: List[List[int]]):
         self.order = order
-        self.faces = faces
-        self.sides = sides
         self.adjMat = adjMat
+        self.faces = len(adjMat)
+        self.sides = len(adjMat[0])
+        self.oppDict = self.findOppFaces()
         self.centers = self.spawn_centers()
         self.edges = self.spawn_edges()
         self.corners = self.spawn_corners()
-        # Lists are mutable in python, modifiying the eges list also odifies the pieces list.
-        self.pieces = self.edges + self.corners
+        self.pieces = self.centers
 
-    def exec_move(self, move: Tuple[int, str, int]):
-        global self.centers, self.edegs, self.corners
-        # move: (count, move, depth)
-        count, move, depth = move
-        for _ in range(count):
-            # Fuce turn.
-            if move == 'F':
-                for piece in self.pieces:
-                    if:
-            elif move == 'B':
+    def exec_alg(self, alg: List[Tuple[str, int]]):
+        # U 0
+        # F 1
+        # R 2
+        # B 3
+        # L 4
+        # D 5
+        # ...
+        for MOVE in alg:
+            # move: (move, depth)
+            move, depth = MOVE
+            # Face turn.
+            if move in 'FBUDLR':
                 pass
-            elif move == 'U':
-                pass
-            elif move == 'D':
-                pass
-            elif move == 'L':
-                pass
-            elif move == 'R':
-                pass
-            # Slice.
-            elif move == 'M':
-                pass
-            elif move == 'E':
-                pass
-            elif move == 'S':
+            # Rotation.
+            elif move in 'xyz':
                 pass
             # Error.
             else:
                 raise AssertionError(move + 'is not a valid move.')
 
-    def exec_alg(self, alg: List[str])):
-        for move in alg:
-            exec_move(move)
+
+    def solved_p(self) -> bool:
+        # Return true if for each face, all the colours of the stickers
+        # of that lies on the face are the same for all pieces of the
+        # puzzle, otherwise return false.
+        return all(len(set(colour for piece in _join_lists(self.edges, self.corners) for colour, pos in zip(piece.colour(), piece.pos()) if pos == face)) == 1 for face in range(self.faces))
+        
+        # # Does not work mypy doesn't (currently) allow + on lists with
+        # # different types of items.
+        # return all(len(set(colour for piece in self.edges + self.corners for colour, pos in zip(piece.colour(), piece.pos()) if pos == face)) == 1 for face in range(self.faces))
 
     def scramble(self):
         moves = 'RBUDLR'
@@ -92,17 +163,17 @@ class Solid:
         for _ in range(len(shape.pieces)):
             exec_move((random.choice(r), random.choice(moves), random.choice(r)))
 
+    def __show_pieces(self, pieces):
+        print([piece.show for piece in pieces])
+
     def show_centers(self):
-        for center in self.centers:
-            print([s.colour for s in center], [s.pos for s in center], [s.depth for s in center])
+        __show_pieces(self.centers)
 
     def show_edges(self):
-        for edge in self.edges:
-            print([s.colour for s in edge], [s.pos for s in edge], [s.depth for s in edge])
+        __show_pieces(self.edges)
 
     def show_corners(self):
-        for corner in self.corners:
-            print([s.colour for s in corner], [s.pos for s in corner], [s.depth for s in corner])
+        __show_pieces(self.corners)
 
     def show_all(self):
         self.show_centers()
@@ -118,20 +189,21 @@ class Tetrahedron(Solid):
     def __init__(self, order: int):
         super().__init__(
             order = order,
-            sides = 3,
-            faces = 4,
             adjMat = [[1,2,3], # 0
                       [0,3,2], # 1
                       [0,1,3], # 2
                       [0,2,1]] # 3
         )
 
+    def rotate(self):
+        # Tetrahedron is the only solid that does not have opposite
+        # faces that are parallel, the rotatate procudure will not work.
+        pass
+
 class Cube(Solid):
     def __init__(self, order: int):
         super().__init__(
             order = order,
-            sides = 4,
-            faces = 6,
             adjMat = [[1,2,3,4], # 0
                       [2,0,4,5], # 1
                       [3,0,1,5], # 2
@@ -177,8 +249,6 @@ class Octahedron(Solid):
     def __init__(self, order: int):
         super().__init__(
             order = order,
-            sides = 3,
-            faces = 8,
             adjMat = [[1,2,3], # 0
                       [0,4,6], # 1
                       [0,6,5], # 2
@@ -193,8 +263,6 @@ class Dodecahedron(Solid):
     def __init__(self, order: int):
         super().__init__(
             order = order,
-            sides = 5,
-            faces = 12,
             adjMat = [[1,2,3,4,5],   # 0
                       [0,5,6,7,2],   # 1
                       [0,1,7,8,3],   # 2
@@ -213,8 +281,6 @@ class Icosahedron(Solid):
     def __init__(self, order: int):
         super().__init__(
             order = order,
-            sides = 3,
-            faces = 20,
             adjMat = [[1,3,18],   # 0
                       [5,0,17],   # 1
                       [3,6,19],   # 2
@@ -236,5 +302,3 @@ class Icosahedron(Solid):
                       [19,16,0],  # 18
                       [15,18,2]]  # 19
         )
-
-print(Tetrahedron(4).show_all())
