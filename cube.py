2fa1e4c0f86997e75a2b28f0c91636ac7c8d1cfd
jholty66 <burholtcharliegmail.com>
2021-01-04 20:07:34 +0000

    sync

--- cube.py
+++ cube.py
@@ -38,8 +38,7 @@ class Piece:
     def depth(self) -> List[int]:
         return [sticker.depth for sticker in self.stickers]
 
-    def show(self):
-        print(self.colour(), self.pos(), self.depth())
+    def show(self): print(self.colour(), self.pos(), self.depth())
 
 class Center(Piece):
     def __init__(self, s1):
@@ -54,16 +53,14 @@ class Corner(Piece):
         super().__init__(s1, s2, s3)
 
 class Move:
-    def __init__(self, depth: str, face: str):
-        self.depth = depth
-        self.face = face
+    def __init__(self, depth: int, face: str):
+        self.depth, self.face = depth, face
 
     def show(self):
         print(self.depth, self.face)
 
 class Solid:
-    def spawn_centers(self) -> List[Center]:
-        return [Center(Sticker(i, int((self.order + 1) / 2))) for i in range(self.faces)] if self.order % 2 == 1 else []
+    def spawn_centers(self) -> List[Center]: return [Center(Sticker(i, int((self.order + 1) / 2))) for i in range(self.faces)] if self.order % 2 == 1 else []
 
     def spawn_edges(self) -> List[Edge]:
         edges: List[Edge] = []
@@ -102,13 +99,14 @@ class Solid:
                     if k not in adj_faces:
                         adj_faces.append(k)
                 if len(adj_faces) == len(self.adj_mat) - 1:
-                    op[i] = [face for face in range(self.faces) if face not in adj_faces][0]
+                    opp_faces[i] = [face for face in range(self.faces) if face not in adj_faces][0]
                     break
         return opp_faces
 
-    def __init__(self, order: int, adj_mat: List[List[int]]):
+    def __init__(self, order: int, adj_mat: List[List[int]], move_face: Dict[str, int]):
         self.order = order
         self.adj_mat = adj_mat
+        self.move_face = move_face # Describes what face a move should turn, varies between solids.
         self.faces = len(adj_mat)
         self.sides = len(adj_mat[0])
         self.oppDict = self.find_opp_faces()
@@ -117,19 +115,72 @@ class Solid:
         self.corners = self.spawn_corners()
         self.pieces = self.centers
 
-    def alg_format(self, alg: str, style: str) -> List[Move]:
-        '''Converts an algorithm string with moves seperated by spaces in either
-WCA, SigN or prefix format and return a list of moves as strings in
-modified prefix format.
+    # Algorihtms functions.
+    # The following procedures shwo left blank to as they are local to objects
+    # of the cube class.
+    # Face turn.
+    def U(self):
+        pass
 
-In the modified format, there are no brackets, wide moves and all moves
-state how many ayers they turn, not just wide moves, even for small
-puzzles like 3x3x3.'''
-        # If brackers have a number at the end, expand the brackets, and
-        # remove them. Eg "(R U R' U')2" expands to "R U R' U' R U R'
-        # U'" Split the alg into strings with backs and strings with
-        # brackets.  Expand the brackets on items with brackets.  Join
-        # the strings.
+    def F(self):
+        pass
+
+    def R(self):
+        pass
+
+    def BR(self):
+        pass
+
+    def B(self):
+        pass
+
+    def BL(self):
+        pass
+
+    def L(self):
+        pass
+
+    def DL(self):
+        pass
+
+    def D(self):
+        pass
+
+    def DR(self):
+        pass
+
+    # Slice.
+    def M(self):
+        pass
+
+    def S(self):
+        pass
+
+    def E(self):
+        pass
+
+    # Rotation.
+    def x(self):
+        pass
+
+    def y(self):
+        pass
+
+    def z(self):
+        pass
+
+    def alg_format(self, alg: str) -> List[Move]:
+        '''Converts an algorithm string with moves seperated by spaces in either WCA, SigN
+or prefix format and return a list of moves as strings in modified prefix
+format.
+
+In the modified format, there are no brackets, wide moves and all moves state
+how many ayers they turn, not just wide moves, even for small puzzles like
+3x3x3.'''
+        # If brackers have a number at the end, expand the brackets, and remove
+        # them. Eg "(R U R' U')2" expands to "R U R' U' R U R' U'" Split the alg
+        # into strings with backs and strings with brackets.  Expand the
+        # brackets on items with brackets.  Join the strings.
         print(alg, ': alg')
         split_alg = re.split(r'(\(.*\)[0-9])', alg)
         print(split_alg, ': split_alg')
@@ -148,27 +199,15 @@ puzzles like 3x3x3.'''
         # List of moves.
         list_alg = re.split('\s+', alg)
         print(list_alg, ': list_alg')
-        assert style in ('WCA', 'SiGN', 'prefix')
-        # Following functions foramt list_alg to prefix format,
-        def WCA(list_alg: List[str]) -> List[str]:
-            format_alg = []
-            for move in list_alg:
-                # Wide move turing 3 or layesrs.
-                if move[0] in '1234567890':
-                    move = move.replace('w', '')
-                # Wide move turining one layer.
-                elif 'w' in move:
-                    move = '2' + move.replace('w', '')
-                format_alg.append(move)
-            return format_alg
-
-        def SiGN(list_alg: List[str]) -> List[str]:
-            return [move.upper() for move in list_alg]
-
-        def prefix(list_alg: List[str]) -> List[str]:
-            return list_alg
-
-        prefix_alg = eval(style)(list_alg)
+        prefix_alg = []
+        for move in list_alg:
+            # Wide move turing 3 or layesrs.
+            if move[0] in '1234567890':
+                move = move.replace('w', '')
+            # Wide move turining one layer.
+            elif 'w' in move:
+                move = '2' + move.replace('w', '')
+            prefix_alg.append(move)
         # Prepend '1' to the beginnig of moves.
         for i in range(len(prefix_alg)):
             if prefix_alg[i] not in '1234567890':
@@ -200,49 +239,31 @@ puzzles like 3x3x3.'''
             else:
                 raise ValueError(move + ' unknown move.')
             for _ in r:
-                format_alg.append(Move(move[0], move[1:]))
+                format_alg.append(Move(int(move[0]), move[1:]))
         print([move.show() for move in format_alg], ': format_alg')
         return format_alg
 
-        def alg_show(self, alg: List[str], type: Callable[[List[str]], str]) -> str:
-            '''Show internal representation of alg in type format.'''
-            pass
+    def alg_show(self, alg: List[str], type: Callable[[List[str]], str]) -> str:
+        '''Show internal representation of alg in type format.'''
+        pass
 
-        def alg_exec(self, alg: List[Tuple[str, int]]):
-            # U 0
-            # F 1
-            # R 2
-            # B 3
-            # L 4
-            # D 5
-            # ...
-            for MOVE in alg:
-                # move: (move, depth)
-                move, depth = MOVE
-                # Face turn.
-                if move in 'FBUDLR':
-                    pass
-                # Rotation.
-                elif move in 'xyz':
-                    pass
-                # Error.
-                else:
-                    raise AssertionError(move + 'is not a valid move.')
-
+    def alg_exec(self, alg: List[Move]):
+        # Defined in child classes.
+        pass
 
     def solved_p(self) -> bool:
-        # Return true if for each face, all the colours of the stickers
-        # of that lies on the face are the same for all pieces of the
-        # puzzle, otherwise return false.
-
-        # Mypy bug fix:
+        # Go over all pices, add the colour of the stickers to a set for each
+        # face.  If the length of the sets are 1, then return true.
         edges: List[Any] = self.edges
-        corners: List[Aly] = self.corners
-        pieces = edges + corners
-        return all(len(set(colour for piece in pieces for colour, pos in zip(piece.colour(), piece.pos()) if pos == face)) == 1 for face in range(self.faces))
-        # # Does not work mypy doesn't (currently) allow + on lists with
-        # # different types of items.
-        # return all(len(set(colour for piece in self.edges + self.corners for colour, pos in zip(piece.colour(), piece.pos()) if pos == face)) == 1 for face in range(self.faces))
+        corners: List[Any] = self.corners
+        face_colour = [None] * self.faces
+        for piece in edges + corners:
+            for colour, pos in zip(piece.colour(), piece.pos()):
+                if face_colour[pos] == None:
+                    face_colour[pos] = colour
+                elif face_colour[pos] != colour:
+                    return False
+        return True
 
     def scramble(self):
         moves = 'RBUDLR'
@@ -277,14 +298,18 @@ class Tetrahedron(Solid):
         super().__init__(
             order = order,
             adj_mat = [[1,2,3], # 0
-                      [0,3,2], # 1
-                      [0,1,3], # 2
-                      [0,2,1]] # 3
+                       [0,3,2], # 1
+                       [0,1,3], # 2
+                       [0,2,1]],# 3
+            move_face = {'U': 0,
+                         'R': 1,
+                         'B': 2,
+                         'L': 3}
         )
 
     def rotate(self):
-        # Tetrahedron is the only solid that does not have opposite
-        # faces that are parallel, the rotatate procudure will not work.
+        # Tetrahedron is the only solid that does not have opposite faces that
+        # are parallel, the rotatate procudure will not work.
         pass
 
 class Cube(Solid):
@@ -292,12 +317,47 @@ class Cube(Solid):
         super().__init__(
             order = order,
             adj_mat = [[1,2,3,4], # 0
-                      [2,0,4,5], # 1
-                      [3,0,1,5], # 2
-                      [4,0,2,5], # 3
-                      [1,0,3,5], # 4
-                      [1,4,3,2]] # 5
+                       [2,0,4,5], # 1
+                       [3,0,1,5], # 2
+                       [4,0,2,5], # 3
+                       [1,0,3,5], # 4
+                       [1,4,3,2]],# 5
+            move_face = {'U': 0,
+                         'F': 1,
+                         'R': 2,
+                         'B': 3,
+                         'L': 4,
+                         'D': 5}
         )
+
+    def face_turn(self, turn):    
+        # Turn a face in the clockwise direction.
+        for p in range(len(self.pieces)):
+            # Sticker of pice lies on the face that is being turned and that
+            # the pice is not a center as they don't change position when being
+            # turned.
+            if any(pos == move_face[turn] for pos in self.pieces[p].pos) and len(self.piece[p]) > 1:
+                self.pieces[p][:] = (s
+                for s in range(len(self.pieces[p][s]):
+                    if s.pos == move_face[
+                map(lambda sticker: sticker.pos = self.adj_mat[self.adj_mat.index(sticker.pos) - 1] if sticker.pos != move_face[piece.pos], piece.sticker)
+    def x(self):
+        self.move_face['B'] = self.move_face['U']
+        self.move_face['U'] = self.move_face['F']
+        self.move_face['F'] = self.move_face['D']
+        self.move_face['D'] = self.adj_mat[self.adj_mat.index(move_face['F']) + 1]
+
+    def y(self):
+        self.move_face['L'] = self.move_face['F']
+        self.move_face['F'] = self.move_face['R']
+        self.move_face['R'] = self.move_face['B']
+        self.move_face['B'] = self.adj_mat[self.adj_mat.index(move_face['R'] + 1)]
+        
+    def z(self):
+        self.move_face['D'] = self.move_face['R']
+        self.move_face['R'] = self.move_face['U']
+        self.move_face['U'] = self.move_face['L']
+        self.move_face['L'] = self.adj_mat[self.adj_mat.index(move_face['U'] + 1)]
 
     scheme = {
        0: 'FFFFFF',
@@ -308,42 +368,19 @@ class Cube(Solid):
        5: 'FFFF00:'
     }
 
-    def show_cube(self):
-        '''
-    uuu
-    uuu
-    uuu
-
-lll fff rrr bbb
-lll fff rrr bbb
-lll fff rrr bbb
-
-    ddd
-    ddd
-    ddd
-        '''
-        print('    ','    ')
-        print('    ','    ')
-        print('    ','    ')
-        print('    ','    ')
-        print('    ','    ')
-        print('    ','    ')
-        print('    ','    ')
-        print('    ','    ')
-        print('    ','    ')
-
 class Octahedron(Solid):
     def __init__(self, order: int):
         super().__init__(
             order = order,
             adj_mat = [[1,2,3], # 0
-                      [0,4,6], # 1
-                      [0,6,5], # 2
-                      [0,5,4], # 3
-                      [3,7,1], # 4
-                      [3,2,7], # 5
-                      [2,1,7], # 6
-                      [4,5,6]] # 7
+                       [0,4,6], # 1
+                       [0,6,5], # 2
+                       [0,5,4], # 3
+                       [3,7,1], # 4
+                       [3,2,7], # 5
+                       [2,1,7], # 6
+                       [4,5,6]],# 7
+            move_face = {}
         )
 
 class Dodecahedron(Solid):
@@ -351,43 +388,72 @@ class Dodecahedron(Solid):
         super().__init__(
             order = order,
             adj_mat = [[1,2,3,4,5],   # 0
-                      [0,5,6,7,2],   # 1
-                      [0,1,7,8,3],   # 2
-                      [0,2,8,9,4],   # 3
-                      [0,3,9,10,5],  # 4
-                      [0,4,10,6,1],  # 5
-                      [1,5,10,11,7], # 6
-                      [1,6,11,8,2],  # 7
-                      [2,7,11,9,3],  # 8
-                      [3,8,11,10,4], # 9
-                      [4,9,11,6,5],  # 10
-                      [10,9,8,7,6]]  # 11
+                       [0,5,6,7,2],   # 1
+                       [0,1,7,8,3],   # 2
+                       [0,2,8,9,4],   # 3
+                       [0,3,9,10,5],  # 4
+                       [0,4,10,6,1],  # 5
+                       [1,5,10,11,7], # 6
+                       [1,6,11,8,2],  # 7
+                       [2,7,11,9,3],  # 8
+                       [3,8,11,10,4], # 9
+                       [4,9,11,6,5],  # 10
+                       [10,9,8,7,6]], # 11
+            move_face = {'U': 0,
+                         'F': 1,
+                         'R': 2,
+                         'BR': 3,
+                         'BL': 4,
+                         'L':5,
+                         'DR': 7,
+                         'DL': 6}
         )
+
+    def x(self):
+        self.move_face['BR'] = self.move_face['U']
+        self.move_face['U'] = self.move_face['F']
+        self.move_face['F'] = self.move_face['D']
+        self.move_face['D'] = self.adj_mat[self.adj_mat.index(move_face['F']) + 1]
+
+    def y(self):
+        self.move_face['L'] = self.move_face['F']
+        self.move_face['F'] = self.move_face['R']
+        self.move_face['R'] = self.move_face['BR']
+        self.move_face['BR'] = self.move_face['BL']
+        self.move_face['BL'] = self.adj_mat[self.adj_mat.index(move_face['R'] + 1)]
+        
+    def z(self):
+        self.move_face['DL'] = self.move_face['DR']
+        self.move_face['DR'] = self.move_face['R']
+        self.move_face['R'] = self.move_face['U']
+        self.move_face['U'] = self.move_face['L']
+        self.move_face['L'] = self.adj_mat[self.adj_mat.index(move_face['U'] + 1)]
 
 class Icosahedron(Solid):
     def __init__(self, order: int):
         super().__init__(
             order = order,
             adj_mat = [[1,3,18],   # 0
-                      [5,0,17],   # 1
-                      [3,6,19],   # 2
-                      [0,4,2],    # 3
-                      [5,7,3],    # 4
-                      [8,4,1],    # 5
-                      [7,10,2],   # 6
-                      [4,9,6],    # 7
-                      [5,12,9],   # 8
-                      [8,11,7],   # 9
-                      [11,15,6],  # 10
-                      [13,10,9],  # 11
-                      [8,17,13],  # 12
-                      [12,14,11], # 13
-                      [13,16,15], # 14
-                      [14,19,10], # 15
-                      [17,18,14], # 16
-                      [16,12,1],  # 17
-                      [19,16,0],  # 18
-                      [15,18,2]]  # 19
+                       [5,0,17],   # 1
+                       [3,6,19],   # 2
+                       [0,4,2],    # 3
+                       [5,7,3],    # 4
+                       [8,4,1],    # 5
+                       [7,10,2],   # 6
+                       [4,9,6],    # 7
+                       [5,12,9],   # 8
+                       [8,11,7],   # 9
+                       [11,15,6],  # 10
+                       [13,10,9],  # 11
+                       [8,17,13],  # 12
+                       [12,14,11], # 13
+                       [13,16,15], # 14
+                       [14,19,10], # 15
+                       [17,18,14], # 16
+                       [16,12,1],  # 17
+                       [19,16,0],  # 18
+                       [15,18,2]], # 19
+            move_face = {}
         )
 
 alg = "R U' R' R (R U R' U')2"
