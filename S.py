ff681e82c52792041e1ac0ddcc643e60293b4bf9
jholty66 <burholtcharlie@gmail.com>
2021-01-27 15:16:02 +0000

    add comment

--- S.py
+++ S.py
@@ -15,19 +15,6 @@ for i,v in enumerate(Dv[:-3]):
         if R<r:r=R
 
 De=[(i,j) for i,v in enumerate(Dv) for j,V in enumerate(Dv) if El(v,V)<r+δ and i!=j]
-print(len(De))
-
-# DM=[[0 for _ in range(len(Dv))] for _ in range(len(Dv))]
-# for e in De:DM[e[0]][e[1]]=1;DM[e[1]][e[0]]=1
-# 
-# Df=[]
-# def DFS(f):
-#    for x,v in enumerate(DM[f[-1]]):
-#        if v:
-#            if len(f)==5:
-#                if x==f[0] and set(f) not in [set(F) for F in Df]:Df.append(f)
-#            elif x not in f: DFS(f+[x])
-# for y in range(len(Dv)):DFS([y])
 
 Df=[]
 def DFS(f):
@@ -37,8 +24,6 @@ def DFS(f):
                 if e[1]==f[0] and set(f) not in [set(F) for F in Df]:Df.append(f)
             elif f[-2] not in e:DFS(f+[e[0]] if e[0]!=f[-1] else f+[e[1]])
 for e in De:DFS(list(e))
-for f in Df:print(f)
-print()
 
 DM=[[Df.index(F) for i in range(len(f)) for F in Df if F!=f and f[i-1] in F and f[i] in F] for f in Df]
 R=range(5);S=set()
