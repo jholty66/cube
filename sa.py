6bf1b716788b5060a8c19f9c61fdc75516ff3d6a
jholty66 <burholtcharlie@gmail.com>
2021-01-27 10:06:57 +0000

    fix

--- sa.py
+++ sa.py
@@ -1,5 +1,4 @@
 import math
-from functools import reduce
 
 δ=0.01
 φ=(1+math.sqrt(5))/2
@@ -15,21 +14,22 @@ for i,v in enumerate(Dv[:-3]):
         R=El(v,V)
         if R<r:r=R
 
-De=set((i,j) for i,v in enumerate(Dv[:-3]) for j,V in enumerate(Dv[i+1:]) if El(v,V)<r+δ)
+De=[(i,j) for i,v in enumerate(Dv) for j,V in enumerate(Dv) if El(v,V)<r+δ and i<j]
 
 Dfe=[]
-def ff(f=[]):
+def DFS(f):
     for e in De:
-        if not f:
-            f=[e]
-        else:
-            if any(v in f[-1] for v in e):
-                if len(f)==5:
-                    if f[0]==e and all(len(set(f)&F)<=1 for F in Dfe) and all([ee for F in Dfe for ee in F].count(E) <= 2 for E in f):
-                        Dfe.append(set(f));f=[]
-                elif e not in f:
-                    ff(f+[e])
-ff()
-print(Dfe)
-#Dfv=[set(v for e in f for v in e[1]) for v in Dfe];print(Dfv)
-#DM=[[Dfe.index(F) for e in f for F in Dfe if e in F and F!=f] for f in Dfe]
+        if any(v in f[-1] for v in e): # check if new edge share verticie with previous edge
+            if len(f)==5:
+                if e==f[0] and set(f) not in [set(F) for F in Dfe]:Dfe.append(f)
+            elif e not in f:DFS(f+[e])
+for e in De:DFS([e])
+print(Dfe);print()
+
+Dfv=[[f[i][0] if f[i][0]!=f[i-1][0] else f[i][1] for i,_ in enumerate(f)] for f in Dfe]
+for f in Dfv:print(f)
+print()
+
+DM=[[Dfe.index(F) for e in f for F in Dfe if e in F and F!=f] for f in Dfe]
+for r in DM:print(r)
+print()
--- sb.py
+++ sb.py
@@ -14,17 +14,25 @@ for i,v in enumerate(Dv[:-3]):
         R=El(v,V)
         if R<r:r=R
 
-De=set((i,j) for i,v in enumerate(Dv) for j,V in enumerate(Dv) if El(v,V)<r+δ and i<j)
+De=[(i,j) for i,v in enumerate(Dv) for j,V in enumerate(Dv) if El(v,V)<r+δ and i<j]
 
-DM=[[0 for _ in range(len(Dv))] for _ in range(len(Dv))]
-for e in De:DM[e[0]][e[1]]=1;DM[e[1]][e[0]]=1
+# DM=[[0 for _ in range(len(Dv))] for _ in range(len(Dv))]
+# for e in De:DM[e[0]][e[1]]=1;DM[e[1]][e[0]]=1
+# 
+# def nf(f):
+#     for x,v in enumerate(DM[f[-1]]):
+#         if v:
+#             if len(f)==5:
+#                 if x==f[0] and set(f) not in [set(F) for F in Df]:Df.append(f)
+#             elif x not in f: nf(f+[x])
+# for y in range(len(Dv)):nf([y])
 
 Df=[]
-def cf(f):
-    for x,v in enumerate(DM[f[-1]]):
-        if v: 
-            if len(f)==5:
-                if f[0]==x:Df.append(f)
-            elif x not in f:cf(f+[x])
-for y in range(len(Dv)):cf([y])
-print(Df,len(Df))
+def DFS(f):
+    for e in De:
+        if len(f)==5:
+            if f[-1] in e and f[0] in e and set(f) not in [set(F) for F in Df]:Df.append(f)
+        elif f[-1] in e and f[-2] not in e:DFS(f+[e[0]] if e[0]!=f[-1] else f+[e[1]])
+for e in De:DFS(list(e))
+
+for f in Df:print(f)
