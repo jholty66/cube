ec54df3439f606ba4c2fda2e70db95b64ba56010
jholty66 <burholtcharlie@gmail.com>
2021-07-13 11:46:16 +0100

    include edges instead of just corners in cube model

--- t.py
+++ t.py
@@ -3,4 +3,5 @@ exec(open('a.py').read());exec(open('d.py').read())
 for f in RL(ff):
  for n in R(1,fl):
   vm=[[[k:=fv[f][fv[f].index(i)-n],vf[k].index(s if s==f else ff[f][ff[f].index(s)-n])] if f in c else [i,j] for j,s in E(c)] for i,c in E(vf)]
-  exec(f"{'ULFBRD'[f]}{n}=lambda c:[[c[i][j] for i,j in r] for r in {vm}]")
+  em=[[[k:=fe[f][fe[f].index(i)-n],ef[k].index(s if s==f else ff[f][ff[f].index(s)-n])] if f in c else [i,j] for j,s in E(c)] for i,c in E(ef)]
+  exec(f"{'ULFBRD'[f]}{n}=lambda c,e:([[c[i][j] for i,j in r] for r in {vm}],[[e[i][j] for i,j in r] for r in {em}])")
