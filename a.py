9c4ceb38db5cf564ec068dada2a8dc36426f5f4b
jholty66 <burholtcharlie@gmail.com>
2021-07-13 11:03:15 +0100

    change compose to work with functions with more than 1 arg

--- a.py
+++ a.py
@@ -1,4 +1,4 @@
 from functools import reduce
 readMove=lambda m:eval(m[0]+('1' if not len(m)-1 else '3' if m[1]=="'" else m[1]))
-compose=lambda *m:lambda x:reduce(lambda f,g:g(f),m,x)
+compose=lambda *m:lambda *x:reduce(lambda f,g:g(*f),m,x)
 readAlg=lambda a:compose(*map(readMove,a.split()))
