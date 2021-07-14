284378cf58c642e937796021a0811c8270607386
jholty66 <burholtcharlie@gmail.com>
2021-07-14 10:37:58 +0100

    add functions to verify if cube is in certian sub-group

--- t.py
+++ t.py
@@ -8,3 +8,9 @@ for f in RL(ff):
   em=[[[k:=fe[f][fe[f].index(i)-n],ef[k].index(s if s==f else ff[f][ff[f].index(s)-n])] if f in c else [i,j] for j,s in E(c)] for i,c in E(ef)]
   exec(f"{faces[f]}{n}=lambda c,e:([[c[i][j] for i,j in r] for r in {vm}],[[e[i][j] for i,j in r] for r in {em}])")
   move=f"{faces[f]}{n}";moves[eval(move)]=move
+
+# cube only
+o=[[faces.index(f) for f in x] for x in 'UD LR FB FB LR UD'.split()]
+G1=lambda VF,EF:all(EF[i][j] in ((0,2,3,5) if ef[i][j] in (0,5) else (2,3,4,1) if ef[i][j] in (1,4) else (0,1,2,3,4,5)) for i in RL(EF) for j in R(2))
+G2=lambda VF,EF:all(VF[i][j] in o[vf[i][j]] for i in RL(vf) for j in R(3) if vf[i][j] in (0,5)) 
+G3=lambda VF,EF:all(VF[i][j] in o[vf[i][j]] for i in RL(vf) for j in R(3)) and all(EF[i][j] in o[ef[i][j]] for i in RL(ef) for j in R(2))
